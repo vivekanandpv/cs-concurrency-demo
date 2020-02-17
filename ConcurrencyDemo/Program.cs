aef489ef5384d2cff7bdc0eab37cc24962be2709
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-17 21:01:14 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -11,21 +11,35 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
-            var thread1 = new Thread(ExecuteTask);
-            var thread2 = new Thread(ExecuteTask);
+            var thread1 = new Thread(delegate()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Anonymous function implementation, Counter: {i}");
+                }
+            });
+            var thread2 = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Delegate implementation, Counter: {i}");
+                }
+            });
 
             thread1.Name = "First Thread";
             thread2.Name = "Second Thread";
 
             thread1.Start();
             thread2.Start();
+
+            ExecuteTask();
         }
 
         private static void ExecuteTask()
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Thread {Thread.CurrentThread.Name}, Counter: {i}");
+                Console.WriteLine($"Method implementation (main thread), Counter: {i}");
             }
         }
     }
