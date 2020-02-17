9c1c0ce0b91efeaca296c744b091c1647025d88d
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-18 00:01:12 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrencyDemo
@@ -10,6 +11,26 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
+            //  Threads are low-level API. With power comes complexity.
+            //  Tasks are higher level abstractions over threads that are
+            //  most suited for I/O bound loads.
+            //  Internally, tasks use ThreadPool hence they are efficient
+            //  Working with the tasks is almost like working with threads
+            //  but much easier
+
+            Task t = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hi there!");
+            });
+
+            t.Start();
+            
+            //  As said already, tasks use ThreadPool, which spawns up
+            //  background threads that are abandoned as soon as the Main Thread
+            //  ends. So don't forget to Wait (similar to thread.Join())
+
+            t.Wait();
         }
     }
 }
