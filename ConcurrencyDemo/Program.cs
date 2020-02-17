0ab4a63f4ce10e2926763c7c7ba9e0ac82f45aa7
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-17 23:06:39 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -11,27 +11,33 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 4; i++)
+            //  Creation of the thread is a costly process
+            //  Every thread has its own stack and context switching overheads as well
+            //  If the developer can keep on creating threads, then the overhead can outweigh
+            //  the performance benefit that could be gained. And the complexity of the application
+            //  is yet another issue to tackle with.
+
+            //  In this light, ThreadPool is a convenient abstraction mechanism which recycles the threads
+            //  One can consider the ThreadPool like a taxi service. After every trip, the taxi is back to
+            //  serve the next customer. As this has to be managed by the CLR, there is not much low-level
+            //  API available.
+
+            ThreadPool.QueueUserWorkItem((state) =>
             {
-                Thread t = new Thread(() =>
-                {
-                    //  By the way, this is the closure
-                    Thread.Sleep(200 * i);
-                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is executing...");
+                Console.WriteLine($"Background Thread? {Thread.CurrentThread.IsBackground}" +
+                                  $"{Environment.NewLine}" +
+                                  $"From ThreadPool? {Thread.CurrentThread.IsThreadPoolThread}" +
+                                  $"{Environment.NewLine}" +
+                                  $"State: {state.ToString()}");
+            }, DateTime.UtcNow);
 
-                });
-
-                //  set t as a background thread
-                t.IsBackground = true;
-
-                //  Start
-                t.Start();
-            }
-
-            Thread.Sleep(500);
-
-            //  When Main Thread exits, the background threads are halted.
-            //  By default, the threads that we create are not background threads.
+            //  Since threads from the thread pool are background threads,
+            //  the process finishes when the foreground thread (i.e., the Main Thread)
+            //  exits. Therefore we delay the completion of Main Thread
+            //  Even then, this behaviour is not predictable. Sometimes, the new thread
+            //  might finish earlier than the main thread. In which case we can see the
+            //  output to the console.
+            Thread.Sleep(1000);
         }
     }
 }
