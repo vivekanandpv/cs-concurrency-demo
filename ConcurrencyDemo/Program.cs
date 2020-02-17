f572a9dd2478490a4472e20eaffed6e331f21cb9
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-17 20:58:30 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrencyDemo
@@ -10,6 +11,22 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
+            var thread1 = new Thread(ExecuteTask);
+            var thread2 = new Thread(ExecuteTask);
+
+            thread1.Name = "First Thread";
+            thread2.Name = "Second Thread";
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        private static void ExecuteTask()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.Name}, Counter: {i}");
+            }
         }
     }
 }
