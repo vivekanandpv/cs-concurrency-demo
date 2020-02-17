cb85d263ddd0eeaf8acc94b1f484ddab1190b3d2
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-17 22:41:04 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrencyDemo
@@ -10,6 +11,27 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 4; i++)
+            {
+                Thread t = new Thread(() =>
+                {
+                    //  By the way, this is the closure
+                    Thread.Sleep(200 * i);
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is executing...");
+
+                });
+
+                //  set t as a background thread
+                t.IsBackground = true;
+
+                //  Start
+                t.Start();
+            }
+
+            Thread.Sleep(500);
+
+            //  When Main Thread exits, the background threads are halted.
+            //  By default, the threads that we create are not background threads.
         }
     }
 }
