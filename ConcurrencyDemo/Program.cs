02fa1deec88b2ee63f300e98c1139e777e0335bc
Vivekanand P V <vivekanandpv@outlook.com>
2020-02-18 13:25:08 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrencyDemo
@@ -10,6 +11,32 @@ namespace ConcurrencyDemo
     {
         static void Main(string[] args)
         {
+            var thread1 = new Thread(() =>
+            {
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine($"Counter is {i}");
+                        Thread.Sleep(100);
+                    }
+                }
+                catch (ThreadAbortException e)
+                {
+                    Console.WriteLine("Aborted");
+                    //Thread.ResetAbort();
+                }
+
+                Console.WriteLine("Thread resumed");
+                Thread.Sleep(1000);
+
+            });
+
+            thread1.Start();
+
+            Thread.Sleep(200);
+            thread1.Abort();
+            thread1.Join();
         }
     }
 }
