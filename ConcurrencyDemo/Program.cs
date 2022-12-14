227fbde736d4fb687fafe4eea1a655873290bf80
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-14 15:05:55 +0530

    done

--- ConcurrencyDemo/Program.cs
+++ ConcurrencyDemo/Program.cs
@@ -9,45 +9,60 @@ namespace ConcurrencyDemo
 {
     class Program
     {
-        //  We want to make the delegate body as a single atomic unit, though it is not.
-        //  We do so by introducing the mutex (mutually exclusive) lock
-        //  It is advisable to use a dedicated reference type object for locks.
-        //  Please read this: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/lock-statement#remarks
+        public static readonly object _la = new object();
+        public static readonly object _lb = new object();
 
-        public static readonly object counterOperationLock = new object();
-
-        public static int Counter = 0;
         static void Main(string[] args)
         {
-            List<Thread> threads = new List<Thread>();
-
-            for (int i = 0; i < 4; i++)
+            Thread t1 = new Thread(() =>
             {
-                Thread t = new Thread(() =>
+                lock (_la)
                 {
-                    //  the effect is more pronounced if the limit is set higher
-                    for (int j = 0; j < 10000; j++)
+                    Foo();
+
+                    Thread.Sleep(1000);
+
+                    lock (_lb)
                     {
-                        lock (counterOperationLock) //  only one thread can enter the block
-                        {
-                            Counter++;  //  This is now artificially atomic!
-                        }
+                        Bar();
                     }
-                });
+                }
+            })
+            { Name = "T1" };
 
-                t.Start();
-                threads.Add(t);
-            }
-
-            foreach (var thread in threads)
+            Thread t2 = new Thread(() =>
             {
-                //  wait for all threads to finish
-                //  otherwise we reach Console.WriteLine early
-                thread.Join();
-            }
+                lock (_lb)
+                {
+                    Bar();
 
-            //  Result is now predictable
-            Console.WriteLine($"Counter: {Counter}");
+                    Thread.Sleep(1000);
+
+                    lock (_la)
+                    {
+                        Foo();
+                    }
+                }
+            })
+            { Name = "T2" };
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Main ends");
+        }
+
+        static void Foo()
+        {
+            Console.WriteLine($"Thread: {Thread.CurrentThread.Name} executes Foo");
+        }
+
+        static void Bar()
+        {
+            Console.WriteLine($"Thread: {Thread.CurrentThread.Name} executes Bar");
         }
     }
 }
